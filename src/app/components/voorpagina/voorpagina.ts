aca43d3ebaaa82da712e4f7294e8f33a28f826c5
justin <12102607@student.pxl.be>
2026-09-01 15:16:16 +0200

    Add voorpagina electrisch en remmon box

--- src/app/components/voorpagina/voorpagina.ts
+++ src/app/components/voorpagina/voorpagina.ts
@@ -9,4 +9,7 @@ import { Component } from '@angular/core';
 export class Voorpagina {
   diagnosesImage = 'images/diagnoses.png';
   onderhoudImage = 'images/onderhoud.png';
+  remmenImage = 'images/remmen.png';
+  electrischImage = 'images/electrisch.png';
+  startImage = 'images/startingbackground.png';
 }
